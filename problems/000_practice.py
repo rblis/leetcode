b0b80136fdbc5ae523079a46dfeb1c859c608be7
Ryan Biswas <rblis@outlook.com>
2023-12-27 13:11:35 -0600

    adds new practice section

--- problems/000_practice.py
+++ problems/000_practice.py
@@ -27,3 +27,25 @@ import math
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         pass
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        vals = list(zip(position, speed))
+        vals.sort(key=lambda x : -x[0])
+        tot = 0
+        print(vals)
+        lastime = (target-vals[0][0])/vals[0][1]
+        for i in range(1,len(position)):
+            time = (target-vals[i][0])/vals[i][1]
+            print(lastime,time)
+            if time <= lastime:
+                vals[i] = (-1,-1)
+                lastime = max(time, lastime)
+            else:
+                lastime = time
+        for val in vals:
+            if val != (-1,-1):
+                tot += 1
+        print(vals)
+        return tot
+    
+
+print(Solution().carFleet(10, [0,4,2], [2,1,3]))
